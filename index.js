d2deeb11a0fbd916861eeb8c169f1d12cf01589f
John Winstead <brooswit@gmail.com>
2017-06-18 11:59:06 -0700

    add config

--- index.js
+++ index.js
@@ -2,17 +2,52 @@ const url = require('url');
 const http = require('http');
 const proxyServer = ( require('http-proxy') ).createProxyServer( );
 
+const config = require('./config.json');
+const port = config.port;
+const routes = config.routes;
+
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
+}
+
 httpServer = http.createServer( (request, response) => {
-  var port = url.parse( request.url ).pathname.split( '/' )[1] === 'api' ? 3030 : 3000;
-  proxyServer.web( request, response, {
-    target: {
-      host: 'localhost',
-      port: port
+  var path = request.url.split('/');
+    path.shift();
+    path = path.join('/');
+
+  var bestRouteDestination;
+  var bestRouteScore=0;
+
+  for(var routeIndex in routes){
+    var route = routes[routeIndex];
+    var score = 0;
+    for (var charIndex in route.path) {
+      if(route.path[charIndex] == path[charIndex]) {
+        route.score ++;
+      } else {
+        charIndex = route.path.length;
+      }
     }
+    if(score>bestRouteScore){
+      bestRouteDestination =  route.destination;
+      bestRouteScore = score;
+    }
+  }
+
+  proxyServer.web(request, response, {
+    target: bestRouteDestination
   });
-}).listen( 8080, ()=>{
-  console.log('Proxy Listening');
-  console.log('"/api/*" => "http://localhost:3030/ (Rails Server)"');
-  console.log('"/*/" => "http://localhost:3000/ (React Dev Environment)"');
-  console.log('');
+}).listen( port, ()=>{
+  console.log('Proxy listening on port {0}'.format(port));
+  routes.forEach((route)=>{
+    console.log('"{0}" => "{1}"'.format(route.path, route.destination));
+  });
 } );
